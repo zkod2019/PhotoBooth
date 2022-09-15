1388ee80e760dcd68cefc3e90f8e20beae734ca1
Zaida Kod <zkod777@gmail.com>
2022-09-15 10:48:54 +0400

    smile btn take a pic with each filter on and saves them all in a folder

--- testing-tkinter.py
+++ testing-tkinter.py
@@ -3,6 +3,8 @@ import threading
 from tkinter import ttk
 
 import picamera
+from picamera import PiCamera
+import time
 
 root = tkinter.Tk()
 root.geometry('300x200')
@@ -15,12 +17,37 @@ def quit_everything():
     root.quit()
     preview_on = False
     
+def take_picture():
+    camera.start_preview()
+    time.sleep(5)
+
+    for effect in camera.IMAGE_EFFECTS:
+        filename = "image_%s.jpg" % effect
+        camera.image_effect = effect
+        camera.capture(filename)
+        time.sleep(1)
+    camera.stop_preview()
+    
 btn = ttk.Button(
 	root,
 	text = 'Quit',
 	command = quit_everything
 )
 
+picBtn = ttk.Button(
+	root,
+	text = 'SMILE!',
+	command = take_picture
+)
+
+filterOneBtn = ttk.Button(
+	root,
+	text = 'Filter 1',
+	command = quit_everything
+)
+
+filterOneBtn.pack(ipadx = 5, ipady = 2, expand = True)
+picBtn.pack(ipadx = 5, ipady = 5, expand = True)
 btn.pack(ipadx = 5, ipady = 5, expand = True)
 
 camera = picamera.PiCamera()
